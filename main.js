10e283d58248e9ce62bb596793e1deebc5ac7750
Richie <rcasto@users.noreply.github.com>
2018-02-25 12:11:41 -0800

    renaming test data output files to match their input.  adding new example.  adjust main test transform file to take in path to transform

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 var adaptiveMarkdown = require('./lib/adaptiveMarkdown');
 
-adaptiveMarkdown.transformFile('./test-data/font-bold.md');
+if (process.argv.length < 3) {
+    console.error('Must pass in path of markdown file to transform');
+    console.error('ex) node main.js path-markdown-file.md');
+    return;
+}
+
+var transformFilePath = process.argv[2];
+adaptiveMarkdown.transformFile(transformFilePath);
